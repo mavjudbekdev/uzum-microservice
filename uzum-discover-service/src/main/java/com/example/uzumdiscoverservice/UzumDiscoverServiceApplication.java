a3e7c3b92cfb718d3c629f24a6dad4070252dec6
developer <mavjudbeksokhibov@gmail.com>
2024-01-30 10:49:16 +0500

    initial commit

---
+++ uzum-discover-service/src/main/java/com/example/uzumdiscoverservice/UzumDiscoverServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.uzumdiscoverservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UzumDiscoverServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UzumDiscoverServiceApplication.class, args);
+    }
+
+}
---
+++ uzum-discover-service/src/test/java/com/example/uzumdiscoverservice/UzumDiscoverServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uzumdiscoverservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UzumDiscoverServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
